8941dfefa163a074eca16eb8ee4a8a9bee0a6230
tahay_deddah <tahya.deddah@cpe.fr>
2021-07-12 17:43:20 -0400

    commit

--- src/Code/ROImean/ROImean.cxx
+++ src/Code/ROImean/ROImean.cxx
@@ -10,49 +10,101 @@
 #include <sstream>      // std::istringstream
 #include <string>   
 #include<fstream>    
+#include <vector>
 
 
 int  main(int argc, char** argv) 
 {
     PARSE_ARGS;
 
-    char     line[70];
+
+    // lists to stock the X measurement and atlas values
     std::vector<std::string> atlas_array;
     std::vector<std::string> measurement_array;
-    std::ifstream inputmeasurement, inputatlas ;
-    inputmeasurement.open(InputMeasurement, ios::in);
-    inputatlas.open(AtlasSurfaceLabeling, ios::in);
-    // read 3 first lines
-    inputmeasurement.getline(line, 500, '\n');
-    inputmeasurement.getline(line, 500, '\n');
-    inputmeasurement.getline(line, 500, '\n');
+    std::string line;
 
-    while (inputatlas.getline(line, 500, '\n'))                          
-    {   atlas_array.push_back(line);  }
-
-    while (inputmeasurement.getline(line, 500, '\n'))                          
-    {   measurement_array.push_back(line);  }
-
-    sort( atlas_array.begin(), atlas_array.end() );
-    atlas_array.erase( std::unique( atlas_array.begin(), atlas_array.end() ), atlas_array.end() );
-
-    int roinum = atlas_array.size();
-    int subjnum = measurement_array.size();
-
-    double ROImean[subjnum][roinum] ;
-    double roisize[roinum][1];
-    double indx[1][1];
-
-
-   /* for (int i=0 ; i<roinum ; i++)
+    std::ifstream atlas_file( AtlasSurfaceLabeling.c_str());
+    if (atlas_file.is_open())
     {
-       roisize[i][0] = 
+        
+        while (std::getline(atlas_file, line)) 
+        {
+            atlas_array.push_back( line); 
+        }
     }
 
-    for i=1:1:roinum;
-        roisize(i)=size(find(atlas==rois(i)),1);
-    end;*/
-	return EXIT_SUCCESS;    
+
+    std::ifstream input_measurement_file( InputMeasurement.c_str());
+    if (input_measurement_file.is_open())
+    {
+        // read three first lines of the csf measurement txt file
+        std::getline(input_measurement_file, line);
+        std::getline(input_measurement_file, line);
+        std::getline(input_measurement_file, line);
+
+        std::string atlas_line;
+        while (std::getline(input_measurement_file, line)) 
+        {
+            measurement_array.push_back( line); 
+        }
+    }
+
+    std::vector<std::string> roi = atlas_array;
+    auto last = std::unique(roi.begin(), roi.end());
+    roi.erase(last, roi.end());
+    std::sort(roi.begin(), roi.end()); 
+    last = std::unique(roi.begin(), roi.end());
+    roi.erase(last, roi.end());
+
+
+    int roinum = roi.size();
+    std::cout << roinum << std::endl;
+    std::cout << measurement_array.size() << std::endl;
+    std::cout << atlas_array.size() << std::endl;
+    double ROI_Mean[roinum] ;
+    int roisize[roinum];
+    int indx[measurement_array.size()];
+
+    // calculate number of times ROI labels appears in atlas to calculate size of ROI
+    for(int i=0; i < roinum ; i++)
+    {
+        int Roi_Number = 0;
+        for(int j=0; j < atlas_array.size() ; j++)
+        {
+            if(atlas_array[j] == roi[i])
+            {
+                Roi_Number = Roi_Number + 1;
+            }
+
+        }
+        roisize[i] = Roi_Number;
+    }
+
+    // For each column of measurement array return the index of roi that correspond to that region in atlas
+    for(int i=0; i < measurement_array.size() ; i++)
+    {
+        for(int j=0; j < roinum ; j++)
+        {
+            if(roi[j] == atlas_array[i]) 
+            {    
+                indx[i] = j;
+            }
+        }
+    }
+
+	// 
+    for(int j=0; j < measurement_array.size() ; j++)
+    {
+        ROI_Mean[indx[j]] = ROI_Mean[indx[j]]+(std::atof(measurement_array[j].c_str())/roisize[indx[j]]);
+        std::cout << ROI_Mean[indx[j]] << std::endl;
+    }
+
+
+
+
+
+
+    return EXIT_SUCCESS;    
 }
 
 
